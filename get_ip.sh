d2740ce35a6dd85782c296854e51bda89af8443c
Negative <cameron.dowd168@gmail.com>
2020-09-15 07:50:42 -0400

    Update get_ip.sh

--- get_ip.sh
+++ get_ip.sh
@@ -9,8 +9,8 @@ tun0=$([ -d /proc/sys/net/ipv4/conf/tun0 ] && echo "tun0: $({ ip -4 -br a sh dev
 
 if ! [ -z "$eth0" ] && ! [ -z "$tun0" ]; then
         echo "$eth0 | $tun0"
-elif [ -z "$eth0" ]; then
-        echo $tun0
 elif [ -z "$tun0" ]; then
         echo $eth0
+elif [ -z "$eth0" ]; then
+        echo $tun0
 fi
